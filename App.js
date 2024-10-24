41d12d922b3598391c4aff53b575513dd742aae8
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-24 19:54:01 +0300

    add information component

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './src/screens/HomeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CoursesScreen from './src/screens/CoursesScreen';
-import CoursesInformaiton from './src/screens/CoursesInformaiton';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./src/screens/HomeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CoursesScreen from "./src/screens/CoursesScreen";
+import CoursesInformaiton from "./src/screens/CoursesInformaiton";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Anasayfa" component={HomeScreen} />
-      <Stack.Screen name="Kurslarım" component={CoursesScreen} />
-      <Stack.Screen
-        name="KursBilgilerim"
-        component={CoursesInformaiton}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Anasayfa" component={HomeScreen} />
+        <Stack.Screen name="Kurslarım" component={CoursesScreen} />
+        <Stack.Screen name="KursBilgilerim" component={CoursesInformaiton} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Kurslarım" onPress={() => navigation.navigate("Kurslarım")} />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
