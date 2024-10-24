4b9d7b124657f8794d93641e9dacc302bfda1ce5
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-24 20:01:32 +0300

    compliceted informationJS

--- components/information.js
+++ components/information.js
@@ -1,12 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function information() {
+export default function information({title, imageSource, desc}) {
   return (
     <View>
-      <Text>information</Text>
+      <Image source={imageSource} style={styles.img}/>
+      <Viev style={styles.descTitle}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.textDesc}>{desc}</Text>
+      </Viev>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    img: {
+        width: '100%',
+      },
+      descTitle: {
+        alignItems: 'center',
+        paddingVertical: 20,
+      },
+      title: {
+        fontSize: 30,
+      },
+      textDesc: {
+        fontSize: 20,
+      },
+})
