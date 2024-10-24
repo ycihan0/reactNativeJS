f9f658f6b80b735bcdb2209984b17c21befddecf
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-10-24 17:25:43 +0300

    navigate

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CoursesScreen from './src/screens/CoursesScreen';
+import CoursesInformaiton from './src/screens/CoursesInformaiton';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Anasayfa" component={HomeScreen} />
+      <Stack.Screen name="Kurslarım" component={CoursesScreen} />
+      <Stack.Screen
+        name="KursBilgilerim"
+        component={CoursesInformaiton}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
